388c6eb9e14338de1755e0436e4f6e76d1cda2d3
Bryan27f <bryanantoniofernandex@gmail.com>
2022-12-16 18:47:45 -0600

    aa

--- View/Generales.php
+++ View/Generales.php
@@ -183,6 +183,13 @@ function footerLogin()
             <script src="../View/js/bootstrap.min.js"></script>'; 
   }
   ?>
+
+
+
+
+
+
+
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
